eb38947f3f7bc2adc7b823a437788303acbf92f5
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-11-21 13:48:48 -0500

    updated

--- 001_team_members/src/App.jsx
+++ 001_team_members/src/App.jsx
@@ -2,18 +2,53 @@ import { useState } from 'react'
 import './App.css'
 import MemberCard from './components/MemberCard'
 
-const membersData = {
-  renji:{name:"Renji", img_source:"https://i.postimg.cc/sgBkfbtx/img-1.jpg"},
-  sora:{name:"Sora", img_source:"https://i.postimg.cc/3RZ6bhDS/img-2.jpg"}
-}
-
+const membersData = [
+  {name:"Renji", key:"renji", img_source:"https://i.postimg.cc/sgBkfbtx/img-1.jpg"},
+  {name:"Sora", key:"sora",img_source:"https://i.postimg.cc/3RZ6bhDS/img-2.jpg"},
+  {name:"Kaito", key:"kaito", img_source:"https://i.postimg.cc/DZhHg0m4/img-3.jpg"},
+  {name:"Tsuki", key:"tsuki", img_source:"https://i.postimg.cc/KjqWx5ft/img-4.jpg"},
+  {name:"Mitsui", key:"mitsui", img_source:"https://i.postimg.cc/nrcWyW4H/img-5.jpg"}
+]
 
 function App() {
 
+  // handling card expansion
+  const [hoverIndex, setHoverIndex] = useState(undefined);
+
+  function handleHoverIn(index){
+    setHoverIndex(index);
+  }
+  function handleHoverOut(){
+    setHoverIndex(undefined);
+  }
+
+  const columnSpans = Array(membersData.length).fill(1);
+  if (hoverIndex != undefined){
+    columnSpans[hoverIndex] = 3;
+  }
+
+  const columnSpanStyle = columnSpans.map(item => `${item}fr`).join(" ");
+  // Adding memebers to the section
+  function addMembers(){
+    return membersData.map(
+        (item, index) => (
+          <MemberCard
+            key={item.key}
+            img_source={item.img_source}
+            mouseIn={() => handleHoverIn(index)}
+            mouseOut={() => handleHoverOut()}
+            selected={hoverIndex===undefined? undefined : hoverIndex==index}
+          />));
+  }
+
+  // return the current component
   return (
-    <div className='container'>
-      <MemberCard img_source={"https://i.postimg.cc/KjqWx5ft/img-4.jpg"}/>
-    </div>
+    <section>
+      <h2>Team Members</h2>
+      <div className='container' style={{gridTemplateColumns: columnSpanStyle}}>
+        {addMembers()}
+      </div>
+    </section>
   )
 }
 
--- 001_team_members/src/components/MemberCard.jsx
+++ 001_team_members/src/components/MemberCard.jsx
@@ -1,9 +1,15 @@
 
-export default function MemberCard({img_source}){
+export default function MemberCard({img_source, selected, mouseIn, mouseOut}){
+
+  let [grayscale, opacity] = [80, 50];
+  if (selected === undefined || selected === true){
+    [grayscale, opacity] = [0, 100];
+    }
 
   return (
-    <div>
-      <img src={img_source}/>
+    <div className="box">
+      <img src={img_source} onMouseOver={mouseIn} onMouseOut={mouseOut}
+          style={{filter:`grayscale(${grayscale}%) opacity(${opacity}%)`}}/>
     </div>
   )
 }
