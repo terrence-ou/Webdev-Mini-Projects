885a5cff0b66b7b07720377fa4924b9d4caffb2e
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-11-28 14:27:00 -0500

    App components reorganized

--- 001_expandable_cards/src/App.jsx
+++ 001_expandable_cards/src/App.jsx
@@ -1,69 +1,30 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { useState } from "react";
-import "./App.css";
-import { ROBOTS } from "./data/data.js";
 import MemberCard from "./components/MemberCard";
+import MembersContainer from "./components/MembersContainer.jsx";
+import MemberInfo from "./components/MemberInfo.jsx";
 
 function App() {
   // handling card expansion
+  const [hoverIndex, setHoverIndex] = useState(undefined);
+
   function handleHoverIn(index) {
     setHoverIndex(index);
   }
+
   function handleHoverOut() {
     setHoverIndex(undefined);
   }
 
-  const [hoverIndex, setHoverIndex] = useState(undefined);
-  const columnSpans = Array(ROBOTS.length).fill(1);
-  if (hoverIndex != undefined) {
-    columnSpans[hoverIndex] = 3;
-  }
-
-  const columnSpanStyle = columnSpans.map((item) => `${item}fr`).join(" ");
-
-  // Adding memeber cards to the section
-  function addRobots() {
-    return ROBOTS.map((item, index) => (
-      <MemberCard
-        key={item.id}
-        img_source={item.img_source}
-        mouseIn={() => handleHoverIn(index)}
-        mouseOut={() => handleHoverOut()}
-        selected={hoverIndex === undefined ? undefined : hoverIndex == index}
-      />
-    ));
-  }
-
-  // Managing the Member introduction
-  let info = <div></div>; // Empty div when no card being selected
-
-  if (hoverIndex !== undefined) {
-    const currRobot = ROBOTS[hoverIndex];
-    info = (
-      <div className="info">
-        <h2>{currRobot.name}</h2>
-        <p>
-          <b>Service ID: </b> {currRobot.id}{" "}
-        </p>
-        <p>
-          <b>Position: </b> {currRobot.position}{" "}
-        </p>
-        <p>{currRobot.description}</p>
-      </div>
-    );
-  }
 
   // return the current component
   return (
     <section>
       <h1>Crew Members</h1>
-      <div
-        className="container"
-        style={{ gridTemplateColumns: columnSpanStyle }}
-      >
-        {addRobots()}
-      </div>
-      {info}
+      <MembersContainer hoverIndex={hoverIndex}
+                        handleHoverIn={handleHoverIn}
+                        handleHoverOut={handleHoverOut}/>
+      <MemberInfo hoverIndex={hoverIndex}/>
     </section>
   );
 }
--- 001_expandable_cards/src/components/MemberCard.jsx
+++ 001_expandable_cards/src/components/MemberCard.jsx
@@ -1,26 +1,39 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/react-in-jsx-scope */
-import "./MemberCard.css";
+import { styled } from "styled-components";
 
-export default function MemberCard({
-  img_source,
-  selected,
-  mouseIn,
-  mouseOut,
-}) {
-  let [grayscale, opacity] = [80, 50];
+
+// Styles
+const Box = styled.div`
+  border-radius: 10px;
+`
+
+const Img = styled.img`
+   width:100%;
+   height:100%;
+   object-fit:cover;
+   transition: 400ms;
+   border-radius: 10px;
+   filter: grayscale(${props => props.$grayscale}) opacity(${props => props.$opacity});
+`
+
+
+export default function MemberCard({ img_source, selected, mouseIn, mouseOut }) {
+
+  let [grayscale, opacity] = [80, 0.5];
+
   if (selected === undefined || selected === true) {
-    [grayscale, opacity] = [0, 100];
+    [grayscale, opacity] = [0, 1.0];
   }
 
   return (
-    <div className="box">
-      <img
+    <Box>
+      <Img
         src={img_source}
         onMouseOver={mouseIn}
         onMouseOut={mouseOut}
-        style={{ filter: `grayscale(${grayscale}%) opacity(${opacity}%)` }}
+        selected={selected}
+        $grayscale={grayscale}
+        $opacity={opacity}
       />
-    </div>
+    </Box>
   );
 }
