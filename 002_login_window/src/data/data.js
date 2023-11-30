9c4d26ba130dfd9276b1465979153c1cf1eca295
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-11-29 22:47:47 -0500

    old files removed

--- 002_login_window/src/data/data.js
+++
@@ -1,7 +0,0 @@
-const UserData = {
-  userA: { password: "123321", group: "user" },
-  userB: { password: "789987", group: "user" },
-  premiumUserA: { password: "234432", group: "premium" },
-  premiumUserB: { password: "678876", group: "premium" },
-};
-export default UserData;
