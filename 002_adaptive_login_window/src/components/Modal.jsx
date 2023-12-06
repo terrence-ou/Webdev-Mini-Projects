335b05d4b8aa327e230b2f7a5dfca59deac45e35
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-12-06 18:33:07 -0500

    day 2 project gif updated

--- 002_adaptive_login_window/src/components/Modal.jsx
+++ 002_adaptive_login_window/src/components/Modal.jsx
@@ -1,47 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
-import { styled } from "styled-components";
-
-const Dialog = styled.dialog`
-
-
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 2rem;
-  width: 15rem;
-  height: 5rem;
-  background-color: #FDFCF6;
-
-  border: none;
-  border-radius: 1rem;
-
-  font-size: 1.0rem;
-  font-weight: 400;
-
-
-  &::backdrop{
-    background-color: black;
-    opacity: 0.5;
-  }
-
-  & button{
-    height: 2.0rem;
-    width: 4.5rem;
-    border: none;
-    border-radius: 0.2rem;
-    background-color: #D88D4E;
-    color: white;
-    transition: .2s;
-
-    &:hover{
-      cursor: pointer;
-      background-color: #A94E1F;
-    }
-  }
-`
 
 const Modal = forwardRef(function Modal({ children }, ref){
 
@@ -53,12 +11,12 @@ const Modal = forwardRef(function Modal({ children }, ref){
   });
 
   return createPortal(
-    <Dialog ref={modalRef}>
+    <dialog ref={modalRef} className="result-modal">
       {children}
       <form method="dialog">
         <button>Close</button>
       </form>
-    </Dialog>,
+    </dialog>,
     document.getElementById("modal")
   )
 
--- 002_adaptive_login_window_old/src/data/data.js
+++
@@ -1,5 +0,0 @@
-const UsersData = {
-  ikari: { password: "123321", group: "user" },
-  atsuka: { password: "123321", group: "premium" },
-};
-export default UsersData;
