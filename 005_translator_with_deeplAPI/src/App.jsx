ffc19f3850936fa5ceb742d96c41cd89895ac57e
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-12-24 16:26:36 -0500

    api connected

--- 005_translator_with_deeplAPI/src/App.jsx
+++ 005_translator_with_deeplAPI/src/App.jsx
@@ -1,7 +1,30 @@
-import Container from "./components/Container.jsx";
+import axios from "axios";
+import { useEffect } from "react";
 
+import Container from "./components/Container.jsx";
+import DEEPL_KEY from "./deeplapi.json";
+
+const URL = "https://api-free.deepl.com/v2/translate";
+const AUTH_KEY = DEEPL_KEY.key;
 
 function App() {
+  
+  useEffect(() => {
+    async function translate(){
+      const response = await axios.post(URL, null, {
+        params: {
+          auth_key: AUTH_KEY,
+          text: "Hello World",
+          target_lang: "DE"
+        }
+      })
+      console.log(response);
+    }
+
+    translate();
+
+  });
+   
 
   return (
     <Container>
