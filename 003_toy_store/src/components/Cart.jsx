da13583e0499c05b60b42342082f9182dc613811
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2024-01-02 14:32:48 -0500

    Cart, Nav files formatted

--- 003_toy_store/src/components/Cart.jsx
+++ 003_toy_store/src/components/Cart.jsx
@@ -2,57 +2,79 @@ import { useContext } from "react";
 import { CartContext } from "./shopping_cart_context";
 import ToysData from "../data/data";
 
-export default function Cart(){
-
+export default function Cart() {
   const { items, updateItemQuantity } = useContext(CartContext);
 
   // Calculate total price in the shopping cart
-  const totalPrice = Object.keys(items).reduce((acc, key) => {
-    return acc + ToysData[key].price * items[key];
-  }, 0).toFixed(2);
+  const totalPrice = Object.keys(items)
+    .reduce((acc, key) => {
+      return acc + ToysData[key].price * items[key];
+    }, 0)
+    .toFixed(2);
 
-  function handleAddItem(key){
+  function handleAddItem(key) {
     updateItemQuantity(key, "ADD");
   }
 
-  function handleSubtractItem(key){
+  function handleSubtractItem(key) {
     updateItemQuantity(key, "SUB");
   }
 
   // Generate a list of items in the cart
-  function getItemsList(){
-    return (<>
-      <ul className="flex flex-col gap-1 content-center my-4 text-[1rem]">
-        {Object.keys(items).map(key => {
-          return (
-          <li key={key} className="flex justify-between content-center">
-            <div className="flex pr-5 w-full justify-between">
-              <span>{ToysData[key].name}</span>
-              <span>${ToysData[key].price}</span>
-            </div>
-            <div className="flex gap-1">
-              <button onClick={()=>handleAddItem(key)} className="w-4 bg-white">+</button>
-              <span className="w-5 text-center">{items[key]}</span>
-              <button onClick={()=>handleSubtractItem(key)} className="w-4 bg-white">-</button>
-            </div>
-          </li>)
-        })}
-      </ul>
-      <p className="text-right">Cart Total: <span className="font-bold pl-4">${totalPrice}</span></p>
-    </>)
+  function getItemsList() {
+    return (
+      <>
+        <ul className="flex flex-col gap-1 content-center my-4 text-[1rem]">
+          {Object.keys(items).map((key) => {
+            return (
+              <li key={key} className="flex justify-between content-center">
+                <div className="flex pr-5 w-full justify-between">
+                  <span>{ToysData[key].name}</span>
+                  <span>${ToysData[key].price}</span>
+                </div>
+                <div className="flex gap-1">
+                  <button
+                    onClick={() => handleAddItem(key)}
+                    className="w-4 bg-white"
+                  >
+                    +
+                  </button>
+                  <span className="w-5 text-center">{items[key]}</span>
+                  <button
+                    onClick={() => handleSubtractItem(key)}
+                    className="w-4 bg-white"
+                  >
+                    -
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+        <p className="text-right">
+          Cart Total: <span className="font-bold pl-4">${totalPrice}</span>
+        </p>
+      </>
+    );
   }
 
   const listEmpty = Object.keys(items).length === 0;
 
-  return (<>
-    <div>
-      {listEmpty && <p>No items in cart!</p>}
-      {!listEmpty && getItemsList()}
-    </div>
-    <form method="dialog" className="flex gap-6 justify-center pt-4 mx-auto text-center">
-      {!listEmpty && <button className="py-1 rounded-md hover:font-bold">Check Out</button>}
-      <button className="hover:font-bold duration-100">Close</button>
-    </form>
+  return (
+    <>
+      <div>
+        {listEmpty && <p>No items in cart!</p>}
+        {!listEmpty && getItemsList()}
+      </div>
+      <form
+        method="dialog"
+        className="flex gap-6 justify-center pt-4 mx-auto text-center"
+      >
+        {!listEmpty && (
+          <button className="py-1 rounded-md hover:font-bold">Check Out</button>
+        )}
+        <button className="hover:font-bold duration-100">Close</button>
+      </form>
     </>
-  )
+  );
 }
--- 003_toy_store/src/components/Nav.jsx
+++ 003_toy_store/src/components/Nav.jsx
@@ -4,40 +4,44 @@ import { useContext, useRef } from "react";
 import CartModal from "./CartModal.jsx";
 import { CartContext } from "./shopping_cart_context";
 
-const buttonClasses = "md:mx-12 sm:mx-6 text-zinc-600 hover:text-zinc-950"
+const buttonClasses = "md:mx-12 sm:mx-6 text-zinc-600 hover:text-zinc-950";
 
-
-export default function Nav(){
+export default function Nav() {
   // Context handling the cart contents
   const { items } = useContext(CartContext);
   const modalRef = useRef();
 
-  const totalItems = Object.values(items).reduce((sum, item) => sum += item, 0);
+  const totalItems = Object.values(items).reduce(
+    (sum, item) => (sum += item),
+    0,
+  );
 
   // open modal when clicking the "cart" button
-  function handleCartClick(){
+  function handleCartClick() {
     modalRef.current.open();
   }
 
-  return <>
-    <CartModal ref={modalRef} />
-    {createPortal(
-    <nav className="relative w-full bg-white h-10 z-20">
-      <div className="flex justify-between h-full max-w-1280 md:px-44 sm:px-20 mx-auto">
-        <h3 className="text-[1.3rem] font-bold tracking-tight my-auto">
-          Tanoshii Toys
-        </h3>
-        <div className="text-base font-semibold text-zinc-950 my-auto">
-          <button className={buttonClasses}>Blog</button>
-          <button className={buttonClasses}>Shop</button>
-          <button className={buttonClasses}>About</button>
-        </div>
-        <div className="my-auto">
-          <button onClick={handleCartClick}>Cart({totalItems})</button>
-        </div>
-      </div>
-    </nav>,
-    document.getElementById("nav")
-  )}
-  </>
+  return (
+    <>
+      <CartModal ref={modalRef} />
+      {createPortal(
+        <nav className="relative w-full bg-white h-10 z-20">
+          <div className="flex justify-between h-full max-w-1280 md:px-44 sm:px-20 mx-auto">
+            <h3 className="text-[1.3rem] font-bold tracking-tight my-auto">
+              Tanoshii Toys
+            </h3>
+            <div className="text-base font-semibold text-zinc-950 my-auto">
+              <button className={buttonClasses}>Blog</button>
+              <button className={buttonClasses}>Shop</button>
+              <button className={buttonClasses}>About</button>
+            </div>
+            <div className="my-auto">
+              <button onClick={handleCartClick}>Cart({totalItems})</button>
+            </div>
+          </div>
+        </nav>,
+        document.getElementById("nav"),
+      )}
+    </>
+  );
 }
