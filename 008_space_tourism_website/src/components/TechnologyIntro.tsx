88105102cb9739737e9ba24c15da28f58f322e3d
Ou, Tingsong <tou31@gatech.edu>
2024-01-29 21:54:53 -0500

    mobile screen bugs fixed

--- 008_space_tourism_website/src/components/TechnologyIntro.tsx
+++ 008_space_tourism_website/src/components/TechnologyIntro.tsx
@@ -10,7 +10,7 @@ const TechnologyIntro = () => {
       <h3 className="font-bellefair text-h3 tablet:text-h3-tablet mobile:text-h3-mobile">
         {currTechData.name.toUpperCase()}
       </h3>
-      <p className="text-violet text-desc tablet:text-desc-tablet mobile:text-desc-mobile font-barlow leading-content tablet:leading-content-tablet mobile:leading-content-mobile max-w-[445px] mobile:w-[327px] mt-[17px]">
+      <p className="text-violet text-desc tablet:text-desc-tablet mobile:text-desc-mobile font-barlow leading-content tablet:leading-content-tablet mobile:leading-content-mobile max-w-[445px] mobile:w-[327px] mobile:pb-[30px] mt-[17px]">
         {currTechData.description}
       </p>
     </section>
