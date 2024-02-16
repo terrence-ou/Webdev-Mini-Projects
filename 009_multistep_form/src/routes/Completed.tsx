827c2b0791a8eb9a5901668e09a847615b46ae35
terrence-ou <outerrencedl@gmail.com>
2024-02-15 21:22:38 -0500

    adaptive layout updated

--- 009_multistep_form/src/routes/Completed.tsx
+++ 009_multistep_form/src/routes/Completed.tsx
@@ -2,10 +2,10 @@ import completedIcon from "../assets/icon-thank-you.svg";
 
 const Completed = () => {
   return (
-    <div className="flex flex-col my-auto items-center w-[450px]">
+    <div className="flex flex-col my-auto items-center w-[450px] mobile:w-full mobile:py-10">
       <img className="w-20 mb-8" src={completedIcon} alt="thank you icon" />
       <h1 className="mb-4 text-heading font-bold text-denim">Thank you!</h1>
-      <p className="text-lg text-grey text-center">
+      <p className="text-lg text-grey text-center mobile:px-4">
         Thanks for confirming your subscription! We hope you have fun using our
         platform. If you ever need support, please feel free to email us at
         support@loremgaming.com
