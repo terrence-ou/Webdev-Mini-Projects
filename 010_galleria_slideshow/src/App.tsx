68a9aa2b769e05f08807d39aeda5f31fdad7b699
terrence-ou <outerrencedl@gmail.com>
2024-02-28 23:41:03 -0500

    configurations updated

--- 010_galleria_slideshow/src/App.tsx
+++ 010_galleria_slideshow/src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+// import data from "./assets/data.json";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
   return (
     <>
       <div>
@@ -29,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ 010_galleria_slideshow/src/hooks/useImage.tsx
@@ -0,0 +1,13 @@
+// import { useState, useEffect } from "react";
+// import { getStorage, getDownloadURL, ref } from "firebase/storage";
+// import { FirebaseApp, initializeApp } from "firebase/app";
+
+const useImage = ({ url }: { url: string }) => {
+  // const [isLoading, setIsLoading] = useState<boolean>(false);
+  // const [image, setImage] = useState<
+  // useEffect(function fectchImage() {}, []);
+  console.log(url);
+  return;
+};
+
+export default useImage;
