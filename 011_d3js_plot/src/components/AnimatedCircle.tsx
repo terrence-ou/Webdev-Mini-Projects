ab2ff4bde3ca7596cf3b3cd00fe0f38958084b4e
terrence-ou <outerrencedl@gmail.com>
2024-03-23 21:54:00 -0400

    AnimatedCircle component simplified

--- 011_d3js_plot/src/components/AnimatedCircle.tsx
+++ 011_d3js_plot/src/components/AnimatedCircle.tsx
@@ -1,18 +1,13 @@
-import { useRef } from "react";
 import { animated, useSpring } from "@react-spring/web";
 
 const AnimatedCirle = ({ cx, cy }: { cx: number; cy: number }) => {
-  const coord = useRef<number[]>([cx, cy]);
   const style = useSpring({
     config: {
       duration: 300,
     },
-    from: {
-      cx: coord.current[0],
-      cy: coord.current[1],
-      fill: cx > 50 ? "tomato" : "cornflowerblue",
-    },
-    to: { cx: cx, cy: cy, fill: cx > 50 ? "tomato" : "cornflowerblue" },
+    cx: cx,
+    cy: cy,
+    fill: cx > 50 ? "tomato" : "cornflowerblue",
   });
 
   return <animated.circle {...style} r="2" />;
