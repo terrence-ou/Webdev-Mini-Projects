44061fdb449ef56df913c200a8c0db82a4882ee4
terrence-ou <outerrencedl@gmail.com>
2024-10-14 19:36:46 -0400

    widgets completed

--- 012_flutter_basics/widgest_learn/lib/main.dart
+++ 012_flutter_basics/widgest_learn/lib/main.dart
@@ -1,57 +1,92 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
-class CounterDisplay extends StatelessWidget {
-  const CounterDisplay({required this.count, super.key});
+class Product {
+  const Product({required this.name});
+  final String name;
+}
 
-  final int count;
+typedef CartChangedCallback = Function(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget {
+  ShoppingListItem(
+      {required this.product,
+      required this.inCart,
+      required this.onCartChanged})
+      : super(key: ObjectKey(product));
+
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+
+  Color _getColor(BuildContext context) {
+    return inCart ? Colors.black45 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle? _getTextStyle(BuildContext context) {
+    if (!inCart) return null;
+    return const TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(width: 120, child: Center(child: Text('Count: $count')));
+    return ListTile(
+        onTap: () {
+          onCartChanged(product, inCart);
+        },
+        leading: CircleAvatar(
+          backgroundColor: _getColor(context),
+          child: Text(product.name[0]),
+        ),
+        title: Text(product.name, style: _getTextStyle(context)));
   }
 }
 
-class CounterIncrementor extends StatelessWidget {
-  const CounterIncrementor({required this.onPressed, super.key});
-
-  final VoidCallback onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-        width: 120,
-        child: ElevatedButton(
-            onPressed: onPressed, child: const Text('Increment')));
-  }
-}
-
-class Counter extends StatefulWidget {
-  const Counter({super.key});
+class ShoppingList extends StatefulWidget {
+  const ShoppingList({required this.products, super.key});
+  final List<Product> products;
 
   @override
-  State<Counter> createState() => _CounterState();
+  State<ShoppingList> createState() => _ShoppingListState();
 }
 
-class _CounterState extends State<Counter> {
-  int _counter = 0;
-  void _increment() {
+class _ShoppingListState extends State<ShoppingList> {
+  final _shoppingCart = <Product>{};
+
+  void _handleCartChanged(Product product, bool inCart) {
     setState(() {
-      ++_counter;
+      if (!inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-      CounterIncrementor(onPressed: _increment),
-      const SizedBox(
-        width: 16,
-      ),
-      CounterDisplay(count: _counter)
-    ]);
+    return Scaffold(
+      appBar: AppBar(title: const Text('Shopping List')),
+      body: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          children: widget.products.map((product) {
+            return ShoppingListItem(
+                product: product,
+                inCart: _shoppingCart.contains(product),
+                onCartChanged: _handleCartChanged);
+          }).toList()),
+    );
   }
 }
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: Center(child: Counter()))));
+  runApp(const MaterialApp(
+      title: 'Shopping App',
+      home: ShoppingList(products: [
+        Product(name: 'Eggs'),
+        Product(name: 'Flour'),
+        Product(name: 'Chocolate chips'),
+      ])));
 }
---
+++ 012_flutter_basics/widgest_learn/lib/main_change_state_separate.dart
@@ -0,0 +1,57 @@
+import "package:flutter/material.dart";
+
+class CounterDisplay extends StatelessWidget {
+  const CounterDisplay({required this.count, super.key});
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(width: 120, child: Center(child: Text('Count: $count')));
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  const CounterIncrementor({required this.onPressed, super.key});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: 120,
+        child: ElevatedButton(
+            onPressed: onPressed, child: const Text('Increment')));
+  }
+}
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+  void _increment() {
+    setState(() {
+      ++_counter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+      CounterIncrementor(onPressed: _increment),
+      const SizedBox(
+        width: 16,
+      ),
+      CounterDisplay(count: _counter)
+    ]);
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(home: Scaffold(body: Center(child: Counter()))));
+}
