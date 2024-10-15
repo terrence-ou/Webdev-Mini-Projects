9cb86e8d7f71334ced686c41b1c268643222076a
terrence-ou <outerrencedl@gmail.com>
2024-10-15 09:47:38 -0400

    constraints and layout added

---
+++ 012_flutter_basics/constraints_learn/android/app/src/main/kotlin/com/example/constraints_learn/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.constraints_learn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ 012_flutter_basics/layout_learn/android/app/src/main/kotlin/com/example/layout_learn/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.layout_learn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
