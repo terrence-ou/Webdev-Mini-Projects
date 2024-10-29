645704f49166a4b633b31ebe0ee8c6185d49bb56
terrence-ou <outerrencedl@gmail.com>
2024-10-28 23:53:14 -0400

    project config updated

--- 012_flutter_basics/provider_shopper/lib/models/cart.dart
+++ 012_flutter_basics/provider_shopper/lib/models/cart.dart
@@ -5,4 +5,23 @@ class CartModel extends ChangeNotifier {
   late CatalogModel _catalog;
   final List<int> _itemIds = [];
   CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    _catalog = newCatalog;
+    notifyListeners();
+  }
+
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+  int get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  void add(Item item) {
+    _itemIds.add(item.id);
+    notifyListeners();
+  }
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+    notifyListeners();
+  }
 }
