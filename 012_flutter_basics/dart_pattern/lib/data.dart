bfb1c7e7fc01c18f55ba9679d636437af67b1753
terrence-ou <outerrencedl@gmail.com>
2024-11-04 21:19:59 -0500

    dart pattern updated

---
+++ 012_flutter_basics/dart_pattern/lib/data.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+
+class Document {
+  final Map<String, Object?> _json;
+  Document() : _json = jsonDecode(documentJson);
+
+  (String, {DateTime modified}) get metadata {
+    if (_json
+        case {
+          'metadata': {'title': String title, 'modified': String localModified}
+        }) {
+      return (title, modified: DateTime.parse(localModified));
+    } else {
+      throw const FormatException('Unexpected JSON');
+    }
+  }
+
+  List<Block> getBlocks() {
+    if (_json case {'blocks': List blocksJson}) {
+      return [for (final blockJson in blocksJson) Block.fromJson(blockJson)];
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
+  }
+}
+
+class Block {
+  final String type;
+  final String text;
+  Block(this.type, this.text);
+
+  factory Block.fromJson(Map<String, dynamic> json) {
+    if (json case {'type': final type, 'text': final text}) {
+      return Block(type, text);
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
+  }
+}
+
+const documentJson = '''
+{
+  "metadata": {
+    "title": "My Document",
+    "modified": "2023-05-10"
+  },
+  "blocks": [
+    {
+      "type": "h1",
+      "text": "Chapter 1"
+    },
+    {
+      "type": "p",
+      "text": "This is just some placeholder text."
+    },
+    {
+      "type": "checkbox",
+      "checked": false,
+      "text": "Learn Dart 3"
+    }
+  ]
+}
+''';
--- 012_flutter_basics/dart_pattern/lib/main.dart
+++ 012_flutter_basics/dart_pattern/lib/main.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/material.dart';
+import 'data.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const DocumentApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class DocumentApp extends StatelessWidget {
+  const DocumentApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+        theme: ThemeData(useMaterial3: true),
+        home: DocumentScreen(
+          document: Document(),
+        ));
+  }
+}
+
+class DocumentScreen extends StatelessWidget {
+  final Document document;
+  const DocumentScreen({required this.document, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final (title, :modified) = document.metadata;
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Column(
+        children: [
+          Center(
+            child: Text('Last Modified $modified'),
+          )
+        ],
       ),
     );
   }
